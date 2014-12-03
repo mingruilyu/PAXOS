412d398cbef5e01a2e4b533a067bfdbb3b86bfdb
mingruilyu <mingruilyu@umail.ucsb.edu>
2014-12-02 20:40:31 -0800

    test

--- Communication.java
+++ Communication.java
@@ -7,24 +7,32 @@ import java.util.*;
 
 class Communication {
 	final static int PORT_BASE = 5000;
-	static private Communication communicationInstance;
+	static private Communication messengerInstance;
 	Map<Integer, Socket> socketMap;
 	/*OutputStream outputStream;
 	InputStream inputStream;
 	OutputStreamWriter outputStreamWriter;
 	InputStreamReader inputStreamReader;
 	BufferedReader bufferedReader;
-	BufferedWriter bufferedWriter;*/	
-	private Communication(Map<Integer, String> ipMap) throws UnknownHostException, IOException {
-		if (ipMap == null) return;
+	BufferedWriter bufferedWriter;*/
+	private Communication() {
 		socketMap = new HashMap<Integer, Socket>();
+	}
+	
+	public void setAddress(Map<Integer, String> ipMap) throws UnknownHostException, IOException {
+		if (ipMap == null) return;
 		for (Integer serNo : ipMap.keySet()) 
 			socketMap.put(serNo, new Socket(ipMap.get(serNo), PORT_BASE + serNo));
-		
+	}
+	
+	public static Communication getMessenger() {
+		if (messengerInstance == null) 
+			return messengerInstance = new Communication();
+		return messengerInstance;
 	}
 	
 	public void sendMessage(Message message) {
-		
+		int a;
 	}
 
 	public Message parseMessage(String message) {
